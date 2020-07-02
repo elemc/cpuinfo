565ffaa5ec0b034336a72289a82c95dea4430562
elemc <me@elemc.name>
2020-07-02 12:14:08 +0300

    added windows get info version and added benchmark

--- cpuinfo_test.go
+++ cpuinfo_test.go
@@ -1,6 +1,7 @@
 package cpuinfo_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/elemc/cpuinfo"
@@ -13,4 +14,19 @@ func TestGet(t *testing.T) {
 	}
 	t.Logf("%#v", info)
 	t.Logf("Sum: %2x", info.Sum())
+}
+
+func BenchmarkGet(b *testing.B) {
+	var prev []byte
+	for i := 0; i < b.N; i++ {
+		info, err := cpuinfo.Get()
+		if err != nil {
+			b.Fatal(err)
+		}
+		sum := info.Sum()
+		if !bytes.Equal(sum[:], prev) && prev != nil {
+			b.Fatal("Checksums not equal")
+		}
+		prev = sum[:]
+	}
 }
---
+++ cpuinfo_windows.go
@@ -0,0 +1,87 @@
+package cpuinfo
+
+import (
+	"os/exec"
+	"regexp"
+
+	"github.com/pkg/errors"
+)
+
+var regexps = map[string]*regexp.Regexp{
+	"vendor":     regexp.MustCompile("^Manufacturer\\n(?P<vendor>.*$)"),
+	"family":     regexp.MustCompile("^Caption\\n.*\\sFamily\\s(?P<family>\\d).*$"),
+	"model":      regexp.MustCompile("^Caption\\n.*Model\\s(?P<model>\\d+).*$"),
+	"model_name": regexp.MustCompile("^Name\\n(?P<model_name>.*$)"),
+	"core_count": regexp.MustCompile("^NumberOfCores\\n(?P<core_count>.*$)"),
+}
+
+func getCommandOutput(wht string) (output []byte, err error) {
+	cmd := exec.Command("wmic", "cpu", "get", what)
+	output, err = cmd.CombinedOutput()
+	if err != nil {
+		err = errors.Wrapf(err, "unable to start (wmic cpu get %s)", what)
+		return
+	}
+	return
+}
+
+func getCommandValue(what string, raw map[string]string) (err error) {
+	output, err := getCommandOutput(what)
+	if err != nil {
+		return
+	}
+	name, value := getValue(output)
+	if name != "" {
+		raw[name] = value
+	}
+	return
+}
+
+func getValue(data []byte) (name, value string) {
+	for key, r := range regexps {
+		if !r.Match(data) {
+			return
+		}
+		fields := r.FindStringSubmatch(string(data))
+		if len(fields) < 2 {
+			continue
+		}
+		name = key
+		value = fields[1]
+	}
+}
+
+func getCPUInfo() (info *CPUInfo, err error) {
+	raw := make(map[string]string)
+
+	// Vendor
+	err = getCommandValue("manufacturer", raw)
+	if err != nil {
+		err = errors.Wrap(err, "unable to get vendor")
+		return
+	}
+
+	// Family Model
+	err = getCommandValue("caption", raw)
+	if err != nil {
+		err = errors.Wrap(err, "unable to get family and model")
+		return
+	}
+
+	// Model name
+	err = getCommandValue("name", raw)
+	if err != nil {
+		err = errors.Wrap(err, "unable to get model name")
+		return
+	}
+
+	// core count
+	err = getCommandValue("numberofcores", raw)
+	if err != nil {
+		err = errors.Wrap(err, "unable to get core count")
+		return
+	}
+
+	info = fromMap(raw)
+	return
+}
